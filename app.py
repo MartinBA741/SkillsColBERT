50e75b3a93b6eee51320cecda8d22df2c3910a6a
Martin Andreasen <ma-bi-an@hotmail.com>
2022-06-06 19:48:22 +0200

    app not dynamic but works without docker-compose

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
 import time
-
-import redis
+#import redis
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+#cache = redis.Redis(host='redis', port=6379)
+#
+#def get_hit_count():
+#    retries = 5
+#    while True:
+#        try:
+#            return cache.incr('hits')
+#        except redis.exceptions.ConnectionError as exc:
+#            if retries == 0:
+#                raise exc
+#            retries -= 1
+#            time.sleep(0.5)
 
 @app.route('/')
 
-
-def hello():
-    count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.\n'
-
 def index():
+    print(hello())
     return render_template('index.html')
 
+def hello():
+    count = 1 #get_hit_count()
+    return f'Hello World! I have been seen {count} times.\n'
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(debug=True)    
